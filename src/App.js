363b54a12f4ebd5a290909198b966beba4ad32ff
Agustin <agustin13agus13@gmail.com>
2024-05-07 19:40:24 -0600

    actividad1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Item from './Componentes/Item/Item';
 import Email from './Componentes/Email/Email';
-import Html from './Componentes/Html/Html';
+
 
 function App() {
   return (
     <div className="App">
-      <Html></Html>
       <Email></Email>
       <Item></Item>
     </div>
